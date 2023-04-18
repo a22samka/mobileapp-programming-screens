efccacd8e343ca2e38053b12a34059b4a9878370
a22samka <a22samka@student.his.se>
2023-04-18 11:20:09 +0200

    Gjorde en Button, Gav den namnet Knapp. Gjorde en ny activity. Gav Knapp ett ID. Efter detta gjorde jag en onClick med en setOnClickListener.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button Knapp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Knapp = findViewById(R.id.button1);
+
+        Knapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+        }
     }
-}
