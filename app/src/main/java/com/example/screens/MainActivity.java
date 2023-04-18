174bc4527bff564b0cf05d91c2ca337f48b263f2
a22samka <a22samka@student.his.se>
2023-04-18 12:20:35 +0200

    Lade till en Intent som kallas Bengt. Jag lade även in Bundle extras för namn och nummer.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -11,17 +11,26 @@ public class MainActivity extends AppCompatActivity {
 
     private Button Knapp;
 
+    private Intent Bengt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Knapp = findViewById(R.id.button1);
+
+
 
         Knapp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                Bengt = new Intent(MainActivity.this, MainActivity2.class);
+                Bengt.putExtra("name", "Arne"); // Optional
+                Bengt.putExtra("number", 55); // Optional
+                startActivity(Bengt);
+
             }
         });
         }
--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -3,12 +3,28 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    String Namn;
+
+    int Nummer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Namn = extras.getString("name");
+            Nummer = extras.getInt("number");
+
+            // Do something with the name and number
+        }
     }
 }
